6943a3a06a5eae7e1f44a147f76307d071ae07ec
abdtyx <1507844577@qq.com>
2023-07-22 21:42:03 +0800

    forge pkts, tx pkts and using self-defined portid

--- main.c
+++ main.c
@@ -39,6 +39,9 @@
 #include <rte_mempool.h>
 #include <rte_mbuf.h>
 
+#include <pcap.h>
+#include <rte_thash.h>
+
 static volatile bool force_quit;
 
 /* MAC updating enabled by default */
@@ -50,26 +53,12 @@ static int mac_updating = 1;
 #define BURST_TX_DRAIN_US 100 /* TX drain every ~100us */
 #define MEMPOOL_CACHE_SIZE 256
 
-/*
- * Configurable number of RX/TX ring descriptors
- */
 #define RTE_TEST_RX_DESC_DEFAULT 1024
 #define RTE_TEST_TX_DESC_DEFAULT 1024
 static uint16_t nb_rxd = RTE_TEST_RX_DESC_DEFAULT;
 static uint16_t nb_txd = RTE_TEST_TX_DESC_DEFAULT;
 
-/* list of enabled ports */
-static uint32_t l2fwd_dst_ports[RTE_MAX_ETHPORTS];
-
-#define MAX_RX_QUEUE_PER_LCORE 16
-#define MAX_TX_QUEUE_PER_PORT 16
-struct lcore_queue_conf {
-	unsigned n_rx_port;
-	unsigned rx_port_list[MAX_RX_QUEUE_PER_LCORE];
-} __rte_cache_aligned;
-struct lcore_queue_conf lcore_queue_conf[RTE_MAX_LCORE];
-
-static struct rte_eth_dev_tx_buffer *tx_buffer[RTE_MAX_ETHPORTS];
+static struct rte_eth_dev_tx_buffer *tx_buffer;
 
 struct rte_mempool * l2fwd_pktmbuf_pool = NULL;
 
@@ -95,7 +84,7 @@ struct hack_pkt_meta {
 
 static inline struct hack_pkt_meta *
 hack_get_pkt_meta(struct rte_mbuf *pkt) {
-        return (struct hack_pkt_meta *)&pkt->udata64;
+	return (struct hack_pkt_meta *)&pkt->udata64;
 }
 
 /* number of package between each print */
@@ -105,15 +94,24 @@ static uint32_t print_delay = 10000000;
 static uint8_t measure_latency = 0;
 static uint32_t latency_packets = 0;
 static uint64_t total_latency = 0;
+static uint16_t destination = 0;
 
 /* Default number of packets: 128; user can modify it by -c <packet_number> in command line */
 static uint32_t packet_number = 0;
+char *pcap_filename = NULL;
 
 #define ONVM_CHECK_BIT(flags, n) !!((flags) & (1 << (n)))
+#define ONVM_SET_BIT(flags, n) ((flags) | (1 << (n)))
 #define SPEED_TESTER_BIT 7
 #define LATENCY_BIT 6
 #define ONVM_NF_ACTION_TONF 2  // send to the NF specified in the argument field (assume it is on the same host)
 #define ONVM_NF_ACTION_DROP 0  // drop packet
+uint16_t port_id = 0;
+#define PORTID port_id
+#define NUM_RX_QUEUE 1
+#define NUM_TX_QUEUE 1
+#define RX_QUEUE_SIZE 20000
+#define TX_QUEUE_SIZE 20000
 
 /*
  * This function displays stats. It uses ANSI terminal codes to clear
@@ -154,7 +152,7 @@ do_stats_display(struct rte_mbuf *pkt) {
 }
 
 static void
-l2fwd_simple_forward(struct rte_mbuf *m, __attribute__((unused)) unsigned portid)
+l2fwd_simple_forward(struct rte_mbuf *m, unsigned portid)
 {
 	static uint32_t counter = 0;
 	if (counter++ == print_delay) {
@@ -181,6 +179,9 @@ l2fwd_simple_forward(struct rte_mbuf *m, __attribute__((unused)) unsigned portid
 			/* Drop real incoming packets */
 			meta->action = ONVM_NF_ACTION_DROP;
 	}
+
+	// After pkt action, transmit it
+	rte_eth_tx_buffer(portid, 0, tx_buffer, m);
 }
 
 /* main processing loop */
@@ -193,7 +194,6 @@ l2fwd_main_loop(void)
 	unsigned lcore_id;
 	uint64_t prev_tsc, diff_tsc, cur_tsc;
 	unsigned i, j, portid, nb_rx;
-	struct lcore_queue_conf *qconf;
 	const uint64_t drain_tsc = (rte_get_tsc_hz() + US_PER_S - 1) / US_PER_S *
 			BURST_TX_DRAIN_US;
 	struct rte_eth_dev_tx_buffer *buffer;
@@ -201,7 +201,6 @@ l2fwd_main_loop(void)
 	prev_tsc = 0;
 
 	lcore_id = rte_lcore_id();
-	qconf = &lcore_queue_conf[lcore_id];
 
 	RTE_LOG(INFO, L2FWD, "entering main loop on lcore %u\n", lcore_id);
 
@@ -215,16 +214,13 @@ l2fwd_main_loop(void)
 		diff_tsc = cur_tsc - prev_tsc;
 		if (unlikely(diff_tsc > drain_tsc)) {
 
-			for (i = 0; i < qconf->n_rx_port; i++) {
+			portid = PORTID;
+			buffer = tx_buffer;
 
-				portid = l2fwd_dst_ports[qconf->rx_port_list[i]];
-				buffer = tx_buffer[portid];
+			sent = rte_eth_tx_buffer_flush(portid, 0, buffer);
+			if (sent)
+				port_statistics[portid].tx += sent;
 
-				sent = rte_eth_tx_buffer_flush(portid, 0, buffer);
-				if (sent)
-					port_statistics[portid].tx += sent;
-
-			}
 
 			prev_tsc = cur_tsc;
 		}
@@ -232,19 +228,16 @@ l2fwd_main_loop(void)
 		/*
 		 * Read packet from RX queues
 		 */
-		for (i = 0; i < qconf->n_rx_port; i++) {
+		portid = PORTID;
+		nb_rx = rte_eth_rx_burst(portid, 0,
+						pkts_burst, MAX_PKT_BURST);
 
-			portid = qconf->rx_port_list[i];
-			nb_rx = rte_eth_rx_burst(portid, 0,
-						 pkts_burst, MAX_PKT_BURST);
+		port_statistics[portid].rx += nb_rx;
 
-			port_statistics[portid].rx += nb_rx;
-
-			for (j = 0; j < nb_rx; j++) {
-				m = pkts_burst[j];
-				rte_prefetch0(rte_pktmbuf_mtod(m, void *));
-				l2fwd_simple_forward(m, portid);
-			}
+		for (j = 0; j < nb_rx; j++) {
+			m = pkts_burst[j];
+			rte_prefetch0(rte_pktmbuf_mtod(m, void *));
+			l2fwd_simple_forward(m, portid);
 		}
 	}
 }
@@ -347,6 +340,209 @@ signal_handler(int signum)
 	}
 }
 
+#define IP_PROTOCOL_TCP 6
+#define IP_PROTOCOL_UDP 17
+
+struct rte_ipv4_hdr*
+onvm_pkt_ipv4_hdr(struct rte_mbuf* pkt) {
+        struct rte_ipv4_hdr* ipv4 =
+                (struct rte_ipv4_hdr*)(rte_pktmbuf_mtod(pkt, uint8_t*) + sizeof(struct rte_ether_hdr));
+
+        /* In an IP packet, the first 4 bits determine the version.
+         * The next 4 bits are called the Internet Header Length, or IHL.
+         * DPDK's ipv4_hdr struct combines both the version and the IHL into one uint8_t.
+         */
+        uint8_t version = (ipv4->version_ihl >> 4) & 0b1111;
+        if (unlikely(version != 4)) {
+                return NULL;
+        }
+        return ipv4;
+}
+
+int
+onvm_pkt_is_ipv4(struct rte_mbuf* pkt) {
+        return onvm_pkt_ipv4_hdr(pkt) != NULL;
+}
+
+struct rte_tcp_hdr*
+onvm_pkt_tcp_hdr(struct rte_mbuf* pkt) {
+        struct rte_ipv4_hdr* ipv4 = onvm_pkt_ipv4_hdr(pkt);
+
+        if (unlikely(
+                ipv4 ==
+                NULL)) {  // Since we aren't dealing with IPv6 packets for now, we can ignore anything that isn't IPv4
+                return NULL;
+        }
+
+        if (ipv4->next_proto_id != IP_PROTOCOL_TCP) {
+                return NULL;
+        }
+
+        uint8_t* pkt_data =
+                rte_pktmbuf_mtod(pkt, uint8_t*) + sizeof(struct rte_ether_hdr) + sizeof(struct rte_ipv4_hdr);
+        return (struct rte_tcp_hdr*)pkt_data;
+}
+
+struct rte_udp_hdr*
+onvm_pkt_udp_hdr(struct rte_mbuf* pkt) {
+        struct rte_ipv4_hdr* ipv4 = onvm_pkt_ipv4_hdr(pkt);
+
+        if (unlikely(
+                ipv4 ==
+                NULL)) {  // Since we aren't dealing with IPv6 packets for now, we can ignore anything that isn't IPv4
+                return NULL;
+        }
+
+        if (ipv4->next_proto_id != IP_PROTOCOL_UDP) {
+                return NULL;
+        }
+
+        uint8_t* pkt_data =
+                rte_pktmbuf_mtod(pkt, uint8_t*) + sizeof(struct rte_ether_hdr) + sizeof(struct rte_ipv4_hdr);
+        return (struct rte_udp_hdr*)pkt_data;
+}
+
+static inline int
+onvm_ft_fill_key(struct onvm_ft_ipv4_5tuple *key, struct rte_mbuf *pkt) {
+        struct rte_ipv4_hdr *ipv4_hdr;
+        struct rte_tcp_hdr *tcp_hdr;
+        struct rte_udp_hdr *udp_hdr;
+
+        if (unlikely(!onvm_pkt_is_ipv4(pkt))) {
+                return -EPROTONOSUPPORT;
+        }
+        ipv4_hdr = onvm_pkt_ipv4_hdr(pkt);
+        memset(key, 0, sizeof(struct onvm_ft_ipv4_5tuple));
+        key->proto = ipv4_hdr->next_proto_id;
+        key->src_addr = ipv4_hdr->src_addr;
+        key->dst_addr = ipv4_hdr->dst_addr;
+        if (key->proto == IP_PROTOCOL_TCP) {
+                tcp_hdr = onvm_pkt_tcp_hdr(pkt);
+                key->src_port = tcp_hdr->src_port;
+                key->dst_port = tcp_hdr->dst_port;
+        } else if (key->proto == IP_PROTOCOL_UDP) {
+                udp_hdr = onvm_pkt_udp_hdr(pkt);
+                key->src_port = udp_hdr->src_port;
+                key->dst_port = udp_hdr->dst_port;
+        } else {
+                key->src_port = 0;
+                key->dst_port = 0;
+        }
+        return 0;
+}
+
+uint8_t rss_symmetric_key[40] = {
+    0x6d, 0x5a, 0x6d, 0x5a, 0x6d, 0x5a, 0x6d, 0x5a, 0x6d, 0x5a, 0x6d, 0x5a, 0x6d, 0x5a,
+    0x6d, 0x5a, 0x6d, 0x5a, 0x6d, 0x5a, 0x6d, 0x5a, 0x6d, 0x5a, 0x6d, 0x5a, 0x6d, 0x5a,
+    0x6d, 0x5a, 0x6d, 0x5a, 0x6d, 0x5a, 0x6d, 0x5a, 0x6d, 0x5a, 0x6d, 0x5a,
+};
+
+/*software caculate RSS*/
+static inline uint32_t
+onvm_softrss(struct onvm_ft_ipv4_5tuple *key) {
+	union rte_thash_tuple tuple;
+	uint8_t rss_key_be[RTE_DIM(rss_symmetric_key)];
+	uint32_t rss_l3l4;
+
+	rte_convert_rss_key((uint32_t *)rss_symmetric_key, (uint32_t *)rss_key_be, RTE_DIM(rss_symmetric_key));
+
+	tuple.v4.src_addr = rte_be_to_cpu_32(key->src_addr);
+	tuple.v4.dst_addr = rte_be_to_cpu_32(key->dst_addr);
+	tuple.v4.sport = rte_be_to_cpu_16(key->src_port);
+	tuple.v4.dport = rte_be_to_cpu_16(key->dst_port);
+
+	rss_l3l4 = rte_softrss_be((uint32_t *)&tuple, RTE_THASH_V4_L4_LEN, rss_key_be);
+
+	return rss_l3l4;
+}
+
+void nf_hack_pkt() {
+	pcap_filename = strdup("virtual_packet.pcap");
+
+	uint32_t i;
+	uint32_t pkts_generated;
+	struct rte_mempool *pktmbuf_pool;
+
+	pkts_generated = 0;
+	pktmbuf_pool = l2fwd_pktmbuf_pool;
+	if (pktmbuf_pool == NULL) {
+		rte_exit(EXIT_FAILURE, "Cannot find mbuf pool!\n");
+	}
+
+	struct rte_mbuf *pkt;
+	pcap_t *pcap;
+	const u_char *packet;
+	struct pcap_pkthdr header;
+	char errbuf[PCAP_ERRBUF_SIZE];
+	uint32_t max_elt_size;
+
+	printf("Replaying %s pcap file\n", pcap_filename);
+
+	pcap = pcap_open_offline(pcap_filename, errbuf);
+	if (pcap == NULL) {
+		fprintf(stderr, "Error reading pcap file: %s\n", errbuf);
+		rte_exit(EXIT_FAILURE, "Cannot open pcap file\n");
+	}
+
+	packet_number = (use_custom_pkt_count ? packet_number : MAX_PKT_NUM);
+	struct rte_mbuf *pkts[packet_number];
+
+	i = 0;
+
+	/* 
+		* max_elt_size is the maximum preallocated memory size permitted for each packet, 
+		* adjusted for the memory offset of the rte_mbuf struct and header/tail lengths
+		*/
+	
+	max_elt_size = pktmbuf_pool->elt_size - sizeof(struct rte_mbuf) - pktmbuf_pool->header_size - pktmbuf_pool->trailer_size;
+
+	// Read from file, not manager
+	while (((packet = pcap_next(pcap, &header)) != NULL) && (i < packet_number)) {
+		struct hack_pkt_meta *pmeta;
+		struct onvm_ft_ipv4_5tuple key;
+
+		/* Length of the packet cannot exceed preallocated storage size */
+		if (header.caplen > max_elt_size) {
+			continue;
+		}
+
+		pkt = rte_pktmbuf_alloc(pktmbuf_pool);
+		if (pkt == NULL)
+				break;
+
+		pkt->pkt_len = header.caplen;
+		pkt->data_len = header.caplen;
+
+		/* Copy the packet into the rte_mbuf data section */
+		rte_memcpy(rte_pktmbuf_mtod(pkt, char *), packet, header.caplen);
+
+		pmeta = hack_get_pkt_meta(pkt);
+		pmeta->destination = destination;
+		pmeta->action = ONVM_NF_ACTION_TONF;
+		pmeta->flags = ONVM_SET_BIT(0, SPEED_TESTER_BIT);
+
+		onvm_ft_fill_key(&key, pkt);
+		pkt->hash.rss = onvm_softrss(&key);
+
+		/* Add packet to batch, and update counter */
+		pkts[i++] = pkt;
+		pkts_generated++;
+	}
+
+	// pkts tx burst
+	for (unsigned int i = 0; i < packet_number; i++) {
+		rte_eth_tx_burst(PORTID, 0, pkts, pkts_generated);
+	}
+
+	/* Exit if packets were unexpectedly not created */
+	if (pkts_generated == 0 && packet_number > 0) {
+		rte_exit(EXIT_FAILURE, "Failed to create packets\n");
+	}
+
+	packet_number = pkts_generated;
+
+}
+
 int
 main(int argc, char **argv)
 {
@@ -381,11 +577,6 @@ main(int argc, char **argv)
 
 	nb_ports = 1;
 
-	/* reset l2fwd_dst_ports */
-	for (portid = 0; portid < RTE_MAX_ETHPORTS; portid++)
-		l2fwd_dst_ports[portid] = 0;
-	last_port = 0;
-
 	if (nb_ports_in_mask % 2) {
 		printf("Notice: odd number of ports in portmask.\n");
 		l2fwd_dst_ports[last_port] = last_port;
@@ -403,6 +594,48 @@ main(int argc, char **argv)
 		rte_exit(EXIT_FAILURE, "Cannot init mbuf pool\n");
 
 	ret = 0;
+
+
+    // 获取第一个可用的port ID
+	port_id = rte_eth_find_next(0);
+    if (port_id >= RTE_MAX_ETHPORTS) {
+        rte_exit(EXIT_FAILURE, "No available port found\n");
+    }
+
+	// 配置port参数
+	struct rte_eth_conf port_conf = {
+        .rxmode = {
+            .mq_mode = ETH_MQ_RX_NONE,
+        },
+        .txmode = {
+            .mq_mode = ETH_MQ_TX_NONE,
+        },
+    };
+    ret = rte_eth_dev_configure(port_id, NUM_RX_QUEUE, NUM_TX_QUEUE, &port_conf);
+    if (ret < 0) {
+        rte_exit(EXIT_FAILURE, "Port configuration failed\n");
+    }
+
+    // 分配内存资源
+    ret = rte_eth_rx_queue_setup(port_id, 0, RX_QUEUE_SIZE, rte_eth_dev_socket_id(port_id), NULL, NULL);
+    if (ret < 0) {
+        rte_exit(EXIT_FAILURE, "Rx queue setup failed\n");
+    }
+    ret = rte_eth_tx_queue_setup(port_id, 0, TX_QUEUE_SIZE, rte_eth_dev_socket_id(port_id), NULL);
+    if (ret < 0) {
+        rte_exit(EXIT_FAILURE, "Tx queue setup failed\n");
+    }
+
+    // 启动port
+    ret = rte_eth_dev_start(port_id);
+    if (ret < 0) {
+        rte_exit(EXIT_FAILURE, "Port start failed\n");
+    }
+
+
+	// forge data packets
+	nf_hack_pkt();
+
 	/* launch per-lcore init on every lcore */
 	rte_eal_mp_remote_launch(l2fwd_launch_one_lcore, NULL, CALL_MASTER);
 	RTE_LCORE_FOREACH_SLAVE(lcore_id) {
